407c677ad84d495df8e1c889056b72b1ba26219d
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2021-03-11 23:59:37 -0500

    Python mysql project created

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+import mysql.connector
+mydb= mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=""
+)
+
+mycursor= mydb.cursor()
+mycursor.execute("CREATE DATABASE mydatabse")
