2cf6fd34c0bee292d055981d1ceb8527b4617ba6
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2021-03-12 00:28:16 -0500

    customer table created

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import mysql.connector
 mydb= mysql.connector.connect(
     host="localhost",
     user="root",
-    password=""
+    password="",
+    database="mydatabase"
 )
 
 mycursor= mydb.cursor()
@@ -11,6 +12,9 @@ mycursor= mydb.cursor()
 
 # show if databse exsist or not
 
-mycursor.execute("SHOW DATABASES")
-for x in mycursor:
-    print(x)
+#mycursor.execute("SHOW DATABASES")
+#for x in mycursor:
+#   print(x)
+
+# table creation
+#mycursor.execute("create table customers (cid INT AUTO_INCREMENT PRIMARY KEY ,name varchar(225) not null, city varchar(225) not null)")
